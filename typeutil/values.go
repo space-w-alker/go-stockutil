3c6f1c9215bd5b78aa054564bf9fe10fc04bca45
Gary Hetzel <garyhetzel@gmail.com>
2017-03-23 18:11:13 -0400

    Adding package typeutil

---
+++ typeutil/values.go
@@ -0,0 +1,72 @@
+package typeutil
+
+import (
+    "reflect"
+    "fmt"
+    "strings"
+)
+
+// Returns whether the given value represents the underlying type's zero value
+func IsZero(value interface{}) bool {
+    if value == nil {
+        return true
+    }
+
+    return reflect.DeepEqual(
+        value,
+        reflect.Zero(reflect.TypeOf(value)).Interface(),
+    )
+}
+
+// Returns whether the given value is "empty" in the semantic sense. Zero values
+// are considered empty, as are arrays, slices, and maps containing only empty
+// values (called recursively). Finally, strings are trimmed of whitespace and
+// considered empty if the result is zero-length.
+//
+func IsEmpty(value interface{}) bool {
+    if IsZero(value) {
+        return true
+    }
+
+    valueV := reflect.ValueOf(value)
+
+    switch valueV.Kind() {
+    case reflect.Array, reflect.Slice:
+        if valueV.Len() == 0 {
+            return true
+        }else{
+            for i := 0; i < valueV.Len(); i++ {
+                if indexV := valueV.Index(i); indexV.IsValid() && !IsEmpty(indexV.Interface()) {
+                    return false
+                }
+            }
+
+            return true
+        }
+
+    case reflect.Map:
+        if valueV.Len() == 0 {
+            return true
+        }else{
+            for _, keyV := range valueV.MapKeys() {
+                if indexV := valueV.MapIndex(keyV); indexV.IsValid() && !IsEmpty(indexV.Interface()) {
+                    return false
+                }
+            }
+
+            return true
+        }
+
+    case reflect.Chan:
+        if valueV.Len() == 0 {
+            return true
+        }
+
+    case reflect.String:
+        if len(strings.TrimSpace(fmt.Sprintf("%v", value))) == 0 {
+            return true
+        }
+    }
+
+    return false
+}
---
+++ typeutil/values_test.go
@@ -0,0 +1,59 @@
+package typeutil
+
+import (
+    "github.com/stretchr/testify/require"
+    "testing"
+)
+
+type Thing struct {
+    Name string
+    Value interface{}
+}
+
+func TestIsZero(t *testing.T) {
+    assert := require.New(t)
+
+    var thing Thing
+    var things []Thing
+    madeThings := make([]Thing, 0)
+
+    assert.True(IsZero(nil))
+    assert.True(IsZero(0))
+    assert.True(IsZero(0.0))
+    assert.True(IsZero(false))
+    assert.True(IsZero(``))
+    assert.True(IsZero(thing))
+    assert.True(IsZero(Thing{}))
+    assert.True(IsZero(things))
+
+    things = append(things, Thing{})
+
+    assert.False(IsZero(1))
+    assert.False(IsZero(0.1))
+    assert.False(IsZero(true))
+    assert.False(IsZero(`value`))
+    assert.False(IsZero(Thing{`value`, true}))
+    assert.False(IsZero(&Thing{}))
+    assert.False(IsZero(things))
+    assert.False(IsZero(madeThings))
+}
+
+
+func TestIsEmpty(t *testing.T) {
+    assert := require.New(t)
+
+    things := make([]Thing, 4)
+    thingmap := make(map[string]Thing)
+    stringmap := map[int]string{
+        1: ``,
+        2: `    `,
+        3: "\t",
+    }
+
+    assert.True(IsEmpty(things))
+    assert.True(IsEmpty(` `))
+    assert.True(IsEmpty(`     `))
+    assert.True(IsEmpty("\t\n  \n\t"))
+    assert.True(IsEmpty(thingmap))
+    assert.True(IsEmpty(stringmap))
+}
