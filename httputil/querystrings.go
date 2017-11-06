43ad14b0cfaccc40920b76b723cb2a43866de15d
Gary Hetzel <garyhetzel@gmail.com>
2017-11-06 17:54:38 -0500

    httputil: Add implicit query unescape to Q* functions

--- httputil/querystrings.go
+++ httputil/querystrings.go
@@ -2,13 +2,14 @@ package httputil
 
 import (
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/ghetzel/go-stockutil/stringutil"
 )
 
 func QInt(req *http.Request, key string, fallbacks ...int64) int64 {
-	if v := req.URL.Query().Get(key); v != `` {
+	if v := Q(req, key); v != `` {
 		if i, err := stringutil.ConvertToInteger(v); err == nil {
 			return i
 		}
@@ -22,7 +23,7 @@ func QInt(req *http.Request, key string, fallbacks ...int64) int64 {
 }
 
 func QFloat(req *http.Request, key string, fallbacks ...float64) float64 {
-	if v := req.URL.Query().Get(key); v != `` {
+	if v := Q(req, key); v != `` {
 		if i, err := stringutil.ConvertToFloat(v); err == nil {
 			return i
 		}
@@ -36,7 +37,7 @@ func QFloat(req *http.Request, key string, fallbacks ...float64) float64 {
 }
 
 func QTime(req *http.Request, key string) time.Time {
-	if v := req.URL.Query().Get(key); v != `` {
+	if v := Q(req, key); v != `` {
 		if i, err := stringutil.ConvertToTime(v); err == nil {
 			return i
 		}
@@ -46,7 +47,7 @@ func QTime(req *http.Request, key string) time.Time {
 }
 
 func QBool(req *http.Request, key string) bool {
-	if v, err := stringutil.ConvertToBool(req.URL.Query().Get(key)); err == nil {
+	if v, err := stringutil.ConvertToBool(Q(req, key)); err == nil {
 		return v
 	}
 
@@ -55,8 +56,12 @@ func QBool(req *http.Request, key string) bool {
 
 func Q(req *http.Request, key string, fallbacks ...string) string {
 	if v := req.URL.Query().Get(key); v != `` {
-		return v
-	} else if len(fallbacks) > 0 {
+		if vS, err := url.QueryUnescape(v); err == nil {
+			return vS
+		}
+	}
+
+	if len(fallbacks) > 0 {
 		return fallbacks[0]
 	} else {
 		return ``
