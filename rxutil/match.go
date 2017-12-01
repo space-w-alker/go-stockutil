0ead864ba628952599e54460167b4a1c32c11a1c
Gary Hetzel <garyhetzel@gmail.com>
2017-11-30 21:51:26 -0500

    rxutil: Only retrieve submatches once per MatchResult

--- rxutil/match.go
+++ rxutil/match.go
@@ -4,8 +4,10 @@ package rxutil
 import "regexp"
 
 type MatchResult struct {
-	rx     *regexp.Regexp
-	source string
+	rx                  *regexp.Regexp
+	source              string
+	leftmost_submatches []string
+	all_submatches      [][]string
 }
 
 // Returns a MatchResult object representing the leftmost match of pattern against
@@ -15,8 +17,10 @@ func Match(source string, pattern string) *MatchResult {
 
 	if rx.MatchString(source) {
 		return &MatchResult{
-			rx:     rx,
-			source: source,
+			rx:                  rx,
+			source:              source,
+			leftmost_submatches: rx.FindStringSubmatch(source),
+			all_submatches:      rx.FindAllStringSubmatch(source, -1),
 		}
 	}
 
@@ -27,17 +31,15 @@ func Match(source string, pattern string) *MatchResult {
 // named capture group (if given a string).  Returns an empty string if the
 // given group name or index does not exist.
 func (self *MatchResult) Group(nameOrIndex interface{}) string {
-	submatches := self.rx.FindStringSubmatch(self.source)
-
 	for i, name := range self.rx.SubexpNames() {
 		switch nameOrIndex.(type) {
 		case string:
 			if i > 0 && name == nameOrIndex.(string) {
-				return submatches[i]
+				return self.leftmost_submatches[i]
 			}
 		case int:
 			if i == nameOrIndex.(int) {
-				return submatches[i]
+				return self.leftmost_submatches[i]
 			}
 		}
 	}
@@ -48,11 +50,10 @@ func (self *MatchResult) Group(nameOrIndex interface{}) string {
 // Returns a map of all named capture matches, keyed on capture group name.
 func (self *MatchResult) NamedCaptures() map[string]string {
 	captures := make(map[string]string)
-	submatches := self.rx.FindStringSubmatch(self.source)
 
 	for i, name := range self.rx.SubexpNames() {
 		if i > 0 && name != `` {
-			captures[name] = submatches[i]
+			captures[name] = self.leftmost_submatches[i]
 		}
 	}
 
@@ -61,7 +62,7 @@ func (self *MatchResult) NamedCaptures() map[string]string {
 
 // Return a slice of all capture groups.
 func (self *MatchResult) Captures() []string {
-	return self.rx.FindStringSubmatch(self.source)
+	return self.leftmost_submatches
 }
 
 // Returns all captures from all matches appended together.  The full match string
@@ -70,7 +71,7 @@ func (self *MatchResult) Captures() []string {
 func (self *MatchResult) AllCaptures() []string {
 	all := make([]string, 0)
 
-	for _, subcaptures := range self.rx.FindAllStringSubmatch(self.source, -1) {
+	for _, subcaptures := range self.all_submatches {
 		if len(subcaptures) > 1 {
 			all = append(all, subcaptures[1:]...)
 		}
