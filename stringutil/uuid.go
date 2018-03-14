1e6fc7e6739192746bf7d01960db3371e4ea6ffe
Gary Hetzel <garyhetzel@gmail.com>
2018-03-14 11:45:12 -0400

    stringutil: Add ParseUUID, MustUUID

--- stringutil/uuid.go
+++ stringutil/uuid.go
@@ -16,6 +16,26 @@ func UuidFromBytes(b []byte) (*Uuid, error) {
 	}
 }
 
+func ParseUUID(in string) (*Uuid, error) {
+	if uuid, err := uuid.Parse(in); err == nil {
+		return &Uuid{
+			UUID: uuid,
+		}, nil
+	} else {
+		return nil, err
+	}
+}
+
+func MustUUID(in string) *Uuid {
+	if uuid, err := uuid.Parse(in); err == nil {
+		return &Uuid{
+			UUID: uuid,
+		}
+	} else {
+		panic(err)
+	}
+}
+
 func UUID() *Uuid {
 	return &Uuid{
 		UUID: uuid.New(),
