b1f887289f2b733af63edebf26d2895f29124291
Gary Hetzel <garyhetzel@gmail.com>
2018-11-11 16:34:44 -0500

    fileutil: Add ReadAllLines(), ReadFirstLine()

--- fileutil/file.go
+++ fileutil/file.go
@@ -3,6 +3,7 @@ package fileutil
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 
 	isatty "github.com/mattn/go-isatty"
 )
@@ -23,6 +24,22 @@ func ReadAll(filename string) ([]byte, error) {
 func ReadAllString(filename string) (string, error) {
 	if data, err := ReadAll(filename); err == nil {
 		return string(data), nil
+	} else {
+		return ``, err
+	}
+}
+
+func ReadAllLines(filename string) ([]string, error) {
+	if data, err := ReadAllString(filename); err == nil {
+		return strings.Split(data, "\n"), nil
+	} else {
+		return nil, err
+	}
+}
+
+func ReadFirstLine(filename string) (string, error) {
+	if lines, err := ReadAllLines(filename); err == nil {
+		return lines[0], nil
 	} else {
 		return ``, err
 	}
