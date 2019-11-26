f06a92cc2a4d77f159c028f0abb608b8f3ca4f40
Gary Hetzel <its@gary.cool>
2019-11-26 14:35:58 -0500

    rxutil: Adding named predefined convenience regexes

---
+++ rxutil/predefined.go
@@ -0,0 +1,16 @@
+package rxutil
+
+import "regexp"
+
+var Numbers = regexp.MustCompile(`-?\d+(?:\.\d+)?`)
+var Integers = regexp.MustCompile(`-?\d+`)
+var NonNegativeIntegers = regexp.MustCompile(`\d+`)
+var Whitespace = regexp.MustCompile(`\w+`)
+var LineBreak = regexp.MustCompile(`(?:\n|\r|\n\r)`)
+var LineBreaks = regexp.MustCompile(`[\n\r]+`)
+var LowerCaseLetter = regexp.MustCompile(`\p{Ll}`)
+var UpperCaseLetter = regexp.MustCompile(`\p{Lu}`)
+var Letter = regexp.MustCompile(`[\p{Lu}\p{Ll}]`)
+var LowerCaseLetters = regexp.MustCompile(`\p{Ll}+`)
+var UpperCaseLetters = regexp.MustCompile(`\p{Lu}+`)
+var Letters = regexp.MustCompile(`[\p{Lu}\p{Ll}]+`)
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package stockutil
 
-const Version = `1.8.7`
+const Version = `1.8.8`
