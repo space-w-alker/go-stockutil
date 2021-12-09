6f8bf1fa5c8fd8c9227d4c028aa9a57e72b28bdc
Gary Hetzel <its@gary.cool>
2021-12-09 13:41:29 -0500

    typeutil: adding ParseSignatureString() and FunctionMatchesSignature()

--- typeutil/reflect.go
+++ typeutil/reflect.go
@@ -1,17 +1,134 @@
 package typeutil
 
+//go:generate peg -inline sigparse.peg
+
 import (
 	"fmt"
 	"reflect"
 )
 
+type functionSignatureGrammarMixin struct {
+}
+
+func (self *functionSignatureGrammarMixin) Hello() string {
+	return Dump(self)
+}
+
+type TypeDeclaration string
+
+func (self TypeDeclaration) String() string {
+	return string(self)
+}
+
+func (self TypeDeclaration) IsSameTypeAs(value interface{}) bool {
+	var t reflect.Type
+
+	if vv, ok := value.(reflect.Value); ok && vv.IsValid() {
+		t = vv.Type()
+	} else if tt, ok := value.(reflect.Type); ok {
+		t = tt
+	} else {
+		t = reflect.TypeOf(value)
+	}
+
+	if t != nil && self != `` {
+		return (t.String() == self.String())
+	}
+
+	return false
+}
+
 // Returns the number of input and return arguments a given function has.
 func FunctionArity(fn interface{}) (int, int, error) {
 	if IsFunction(fn) {
-		fnT := reflect.TypeOf(fn)
+		var fnT = reflect.TypeOf(fn)
 
 		return fnT.NumIn(), fnT.NumOut(), nil
 	} else {
 		return 0, 0, fmt.Errorf("expected function, got %T", fn)
+	}
+}
+
+// Parse the given function signature string and return the function name, input, and output arguments.
+// Example: "helloWorld(string) error" would return an ident of "helloWorld", a 1-element type declaration
+// representing the "string" argument, and a 1-element returns array with the "error" return parameter.
+func ParseSignatureString(signature string) (ident string, args []TypeDeclaration, returns []TypeDeclaration, perr error) {
+	var grammar = &typeutilFunctionSignatureSpec{
+		Buffer: signature,
+		Pretty: true,
+	}
+
+	if err := grammar.Init(); err != nil {
+		perr = err
+		return
+	}
+
+	if err := grammar.Parse(); err != nil {
+		perr = err
+		return
+	}
+
+	var decls = make([]TypeDeclaration, 0)
+
+	for _, token := range grammar.Tokens() {
+		switch rule := token.pegRule; rule {
+		case ruleKW_FUNC:
+			ident = `(anonymous)`
+		case ruleIDENT:
+			ident = signature[token.begin:token.end]
+		case ruleSIGNATURE:
+			args = make([]TypeDeclaration, len(decls))
+			copy(args, decls)
+			decls = make([]TypeDeclaration, 0)
+		case ruleRETURNS:
+			returns = make([]TypeDeclaration, len(decls))
+			copy(returns, decls)
+			decls = nil
+		case ruleDATATYPE:
+			decls = append(decls, TypeDeclaration(signature[token.begin:token.end]))
+		}
+	}
+
+	return
+}
+
+// Returns whether the given function's actual signature matches the given spec string (as parsed by
+// ParseSignatureString).
+func FunctionMatchesSignature(fn interface{}, signature string) error {
+	fn = ResolveValue(fn)
+	var fnT = reflect.ValueOf(fn).Type()
+
+	if fnT.Kind() != reflect.Func {
+		return fmt.Errorf("expected function, got %T", fn)
+	}
+
+	if _, args, returns, err := ParseSignatureString(signature); err == nil {
+		if len(args) != fnT.NumIn() {
+			return fmt.Errorf("expected %d arguments, got %d", len(args), fnT.NumIn())
+		}
+
+		if len(returns) != fnT.NumOut() {
+			return fmt.Errorf("expected %d return arguments, got %d", len(returns), fnT.NumOut())
+		}
+
+		for i, arg := range args {
+			var fnArg = fnT.In(i)
+
+			if !arg.IsSameTypeAs(fnArg) {
+				return fmt.Errorf("argument %d type mismatch: expected %v, got %v", i, arg, fnArg)
+			}
+		}
+
+		for i, arg := range returns {
+			var fnArg = fnT.Out(i)
+
+			if !arg.IsSameTypeAs(fnArg) {
+				return fmt.Errorf("return argument %d type mismatch: expected %v, got %v", i, arg, fnArg)
+			}
+		}
+
+		return nil
+	} else {
+		return err
 	}
 }
--- typeutil/reflect_test.go
+++ typeutil/reflect_test.go
@@ -1,11 +1,14 @@
 package typeutil
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
 	"github.com/ghetzel/testify/require"
 )
+
+type testCustomType struct{}
 
 func TestFunctionArity(t *testing.T) {
 	assert := require.New(t)
@@ -26,4 +29,49 @@ func TestFunctionArity(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(0, i)
 	assert.Equal(1, o)
+}
+
+func TestParseSignatureString(t *testing.T) {
+	//----------------------------------------------------------------------------------------------
+	var ident, args, returns, err = ParseSignatureString(`testFunc(str,bool, *testCustomType) (bool,error)`)
+	require.NoError(t, err)
+	require.Equal(t, `testFunc`, ident)
+	require.Len(t, args, 3)
+	args[0].IsSameTypeAs(`example string`)
+	args[1].IsSameTypeAs(true)
+	args[2].IsSameTypeAs(new(testCustomType))
+	require.Len(t, returns, 2)
+	returns[0].IsSameTypeAs(true)
+	returns[1].IsSameTypeAs(errors.New(`test error`))
+
+	//----------------------------------------------------------------------------------------------
+	ident, args, returns, err = ParseSignatureString(`func()`)
+	require.NoError(t, err)
+	require.Equal(t, `(anonymous)`, ident)
+	require.Len(t, args, 0)
+	require.Len(t, returns, 0)
+
+	//----------------------------------------------------------------------------------------------
+	ident, args, returns, err = ParseSignatureString(`testFunc() error`)
+	require.NoError(t, err)
+	require.Equal(t, `testFunc`, ident)
+	require.Len(t, args, 0)
+	require.Len(t, returns, 1)
+	returns[0].IsSameTypeAs(errors.New(`test error`))
+
+	//----------------------------------------------------------------------------------------------
+	ident, args, returns, err = ParseSignatureString(`testFunc(bool) (error)`)
+	require.NoError(t, err)
+	require.Equal(t, `testFunc`, ident)
+	require.Len(t, args, 1)
+	args[0].IsSameTypeAs(false)
+	require.Len(t, returns, 1)
+	returns[0].IsSameTypeAs(errors.New(`test error`))
+}
+
+func TestFunctionMatchesSignature(t *testing.T) {
+	require.NoError(t, FunctionMatchesSignature(func() {}, `func()`))
+	require.NoError(t, FunctionMatchesSignature(func(_ string) {}, `func(string)`))
+	require.NoError(t, FunctionMatchesSignature(func(_ string) error { return nil }, `func(string) error`))
+	require.NoError(t, FunctionMatchesSignature(func(_ string) (int, error) { return 0, nil }, `func(string) (int, error)`))
 }
